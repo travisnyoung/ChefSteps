8df8afe722c5e02a38b4985d777fbbb0fa208b3f
Travis Young <travis.n.young@gmail.com>
2018-08-03 17:02:39 -0700

    Initial Commit

--- deDupe.py
+++ deDupe.py
@@ -2,10 +2,17 @@
 #This is the core. It's pretty basic, make a hashable array and 
 def dedupe_emails(emails_list):
 	deduped_emails = {}
-	count = 0
 	for each in emails_list:
-		count += 1
 		if each not in deduped_emails:
 			deduped_emails[each] = ""
 
 	return list(iter(deduped_emails.keys()))
+
+"""
+TODO:
+Need a function that can take a JSON POST and turn that into the list being de-
+duped, then one that takes the results and turn them back into a JSON array to
+be handed back to the UI/further business logic (amazon SMS maybe?)
+
+Also probably some preliminary validation ("is this an email address?")
+"""
